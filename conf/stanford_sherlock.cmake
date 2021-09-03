1b4fe40b0445e93aba317ae725433f9afe8fab12
Oguz Ziya Tikenogullari <oguzziya@sh03-ln05.stanford.edu>
2021-09-03 07:46:59 -0700

    sherlock config

--- conf/stanford_sherlock.cmake
+++ conf/stanford_sherlock.cmake
@@ -1,40 +1,51 @@
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-
 # =========================================================
 # 1. VTK VARIABLES
 # =========================================================
-SET(VTK_DIR /home/groups/amarsden/lib-perigee/VTK-7.1.1-shared)
+set(VTK_DIR /home/groups/amarsden/lib-perigee/VTK-7.1.1-shared/lib/cmake/vtk-7.1)
 
-#----------------------------------------------------------
-SET(VTK_VERSION vtk-7.1)
-SET(VTK_link_lib vtkCommonCore-7.1 vtkCommonSystem-7.1 vtkCommonDataModel-7.1
-  vtkCommonExecutionModel-7.1 vtkCommonMisc-7.1 vtkCommonTransforms-7.1
-  vtkCommonMath-7.1 vtkIOCore-7.1 vtkIOLegacy-7.1 vtkIOXML-7.1 vtksys-7.1 
-  vtkzlib-7.1 )
-#----------------------------------------------------------
+set(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.11.3-opt)
+set(PETSC_ARCH .)
 
-# ========================================================
-# 2. PETSc VARIABLES
-# ========================================================
-#SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.11.3)
-#SET(PETSC_ARCH arch-linux2-c-opt)
+set(METIS_DIR /home/groups/amarsden/lib-perigee/metis-5.0.3)
 
-SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt)
-SET(PETSC_ARCH .)
-
-find_package(PETSc)
+set(HDF5_ROOT /home/groups/amarsden/lib-perigee/hdf5-1.8.16)
 
 # ========================================================
-# 4. HDF5 VARIABLES
+# setup the libraries
 # ========================================================
-# Modify the HDF5_DIR
-SET(HDF5_DIR /home/groups/amarsden/lib-perigee/hdf5-1.8.16)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
+find_package(VTK REQUIRED)
+find_package(PETSc REQUIRED)
+find_package(HDF5 REQUIRED)
+
+include_directories(${VTK_INCLUDE_DIRS})
+include_directories(${PETSC_INC})
+
+set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${VTK_LIBRARIES})
+set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${PETSC_LIB})
+
+if(PETSC_METIS)
+  set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${PETSC_METIS_LIB})
+  message(STATUS "Use METIS in PETSc: " ${PETSC_METIS_LIB})
+else(PETSC_METIS)
+  find_package(METIS)
+  INCLUDE_DIRECTORIES(${METIS_INCLUDE_DIRS})
+  set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${METIS_LIBRARIES})
+endif(PETSC_METIS)
+
+include_directories(${HDF5_INCLUDE_DIRS})
+set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${HDF5_LIBRARIES})
+
+message(STATUS "External Libraries: " ${EXTRA_LINK_LIBS})
 
 # ========================================================
-# 5. Compiler options 
+# Compiler options 
 # ========================================================
-SET(CMAKE_C_COMPILER  /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt/bin/mpicc)
-SET(CMAKE_CXX_COMPILER /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt/bin/mpicxx)
+SET(CMAKE_C_COMPILER  /home/groups/amarsden/lib-perigee/petsc-3.11.3-opt/bin/mpicc)
+SET(CMAKE_CXX_COMPILER /home/groups/amarsden/lib-perigee/petsc-3.11.3-opt/bin/mpicxx)
+SET(CMAKE_CXX_STANDARD 11)
 
 IF( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
   SET(CMAKE_CXX_FLAGS "-O3 -Wall")
@@ -42,6 +53,6 @@ ELSE( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
   SET(CMAKE_CXX_FLAGS "-O0 -Wall")
 ENDIF( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
 
-SET(CMAKE_CXX_STANDARD 11)
+
 
 # EOF
